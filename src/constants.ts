fafac2f68a84090ec19ef70ce46cf910985296a4
Olaf Neumann <git@olafneumann.org>
2026-03-08 22:31:13 +0100

    Add constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+
+export const WORD_LIST_URI = "/en/5.txt"
+export const MAX_CHARACTERS = 5;
+export const ALLOWED_CHARACTERS_REGEX = /^[A-Z]$/;
+export const DISALLOWED_CHARACTERS_REGEX = /[^A-Z]/gs;
